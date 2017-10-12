c00a912cfdac1fd2c518371b9ee55322b01e39a5
trisberg <trisberg@pivotal.io>
2017-10-11 22:08:26 -0400

    Cleanup and polish
    - add zipkin to teardown
    
    - comment out tests until we have JUnit Kafka Rule
    
    - add missing license headers

--- topic-gateway/src/main/java/io/sk8s/topic/gateway/MessageTraceHandler.java
+++ topic-gateway/src/main/java/io/sk8s/topic/gateway/MessageTraceHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.sk8s.topic.gateway;
 
 import java.util.HashMap;
@@ -15,6 +31,10 @@ import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.messaging.support.NativeMessageHeaderAccessor;
 import org.springframework.util.StringUtils;
 
+/**
+ * @author Thomas Risberg
+ * @author Marcin Grzejszczak
+ */
 public class MessageTraceHandler {
 
 	private Tracer tracer;
@@ -31,7 +51,8 @@ public class MessageTraceHandler {
 		MessageBuilder<?> messageBuilder = MessageBuilder.fromMessage(message);
 
 		Span parentSpan = tracer.getCurrentSpan();
-		Span span = tracer.createSpan(topicName, parentSpan);
+		String spanName = topicName + ":output";
+		Span span = parentSpan != null ? tracer.createSpan(spanName, parentSpan) : tracer.createSpan(spanName);
 		span.logEvent(Span.CLIENT_SEND);
 		messageBuilder.setHeader("messageSent", true);
 
