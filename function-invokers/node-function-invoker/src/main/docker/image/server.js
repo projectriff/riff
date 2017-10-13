75144afb2c65323e6d97099647d2ed4dfc7e2142
Eric Bottard <eric.bottard+github@gmail.com>
2017-10-13 18:05:31 +0200

    Address review comments

--- function-invokers/node-function-invoker/src/main/docker/image/server.js
+++ function-invokers/node-function-invoker/src/main/docker/image/server.js
@@ -10,7 +10,7 @@ app.use('/', bodyParser.text());
 var fn = require(process.env.FUNCTION_URI);
 
 app.post('/', function (req, res) {
-	var resultx = fn(req.body);
+    var resultx = fn(req.body);
     console.log("Result " + resultx);
     res.type("text/plain");
     res.status(200).send(resultx);
