6c5a26cbc8bee5409bbad22843f0d5f4facaa980
markfisher <mfisher@pivotal.io>
2017-10-19 17:40:12 -0400

    reorganizing samples

--- function-invokers/shell-function-invoker/src/main/docker/image/runner.sh
+++ function-invokers/shell-function-invoker/src/main/docker/image/runner.sh
@@ -8,6 +8,9 @@ do
 	sleep 1
 done
 
+# todo: only use this as a default if no env var set
+FUNCTION_URI=/functions/script.sh
+
 while read X; do
 	source "$FUNCTION_URI" "$X";
 done </pipes/input >/pipes/output
