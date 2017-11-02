bc7906ff74fccfd563230182c7315629287c1bd8
markfisher <mfisher@pivotal.io>
2017-11-02 17:38:05 -0400

    Added support for sync request/reply
    - using embedded headers mode until we can support Kafka 0.11 in the sidecar
    - handling any message payload type for replies

--- topic-gateway/src/main/java/io/sk8s/topic/gateway/TopicGatewayController.java
+++ topic-gateway/src/main/java/io/sk8s/topic/gateway/TopicGatewayController.java
@@ -18,30 +18,82 @@ package io.sk8s.topic.gateway;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.UUID;
+import java.util.WeakHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.MonoProcessor;
 
 /**
  * @author Mark Fisher
  */
 @RestController
+@EnableBinding(Sink.class)
 public class TopicGatewayController {
+
+	private final Logger logger = LoggerFactory.getLogger(TopicGatewayController.class);
 
 	@Autowired
 	private MessagePublisher publisher;
 
+	private final Map<String, MonoProcessor<Object>> replies = new WeakHashMap<>();
+
 	@PostMapping("/messages/{topic}")
-	public String publishMessage(@PathVariable String topic, @RequestBody String payload) throws UnsupportedEncodingException {
+	public String publishMessage(@PathVariable String topic, @RequestBody String payload,
+			@RequestHeader("Content-Type") String contentType) throws UnsupportedEncodingException {
 		Message<byte[]> message = MessageBuilder.withPayload(payload.getBytes(StandardCharsets.UTF_8.name()))
 				.setHeader("topic", topic)
+				.setHeader(MessageHeaders.CONTENT_TYPE, contentType)
 				.build();
 		this.publisher.publishMessage(topic, message);
 		return "message published to topic: " + topic + "\n";
+	}
+
+	@PostMapping("/requests/{topic}")
+	public Mono<Object> publishRequest(@PathVariable String topic, @RequestBody String payload,
+			@RequestHeader("Content-Type") String contentType) throws UnsupportedEncodingException {
+		String correlationId = UUID.randomUUID().toString();
+		Message<byte[]> message = MessageBuilder.withPayload(payload.getBytes(StandardCharsets.UTF_8.name()))
+				.setHeader("topic", topic)
+				.setHeader(MessageHeaders.CONTENT_TYPE, contentType)
+				.setHeader("correlationId", correlationId)
+				.build();
+		MonoProcessor<Object> reply = MonoProcessor.create();
+		this.replies.put(correlationId, reply);
+		this.publisher.publishMessage(topic, message);
+		this.logger.debug("Message published to '{}' with correlationId: {}", topic, correlationId);
+		return reply;
+	}
+
+	@StreamListener(Sink.INPUT)
+	public void handleReply(Message<Object> reply) {
+		String correlationId = reply.getHeaders().get("correlationId", String.class);
+		if (StringUtils.hasText(correlationId)) {
+			MonoProcessor<Object> replyHolder = this.replies.remove(correlationId);
+			if (replyHolder != null) {
+				replyHolder.onNext(reply.getPayload());
+			}
+			else {
+				logger.debug("Received reply for timed out request: " + reply);
+			}
+		}
 	}
 }
