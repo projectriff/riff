6fee837ecde02fef07a83d4e0b278213a2a2cff3
David Turanski <dturanski@pivotal.io>
2017-12-01 11:27:19 -0500

    Include instructions to run locally

--- samples/node/slack/slack-command/slack_command.js
+++ samples/node/slack/slack-command/slack_command.js
@@ -1,7 +1,1 @@
-module.exports = function(input) {
-	/*
-	 * This will echo plain text or the contents of the text property, which
-	 * is what slack command POSTs.
-	 */
-	 return "received:" + (typeof(input) == "string" ? input : input.text);
-};
+module.exports = (input) => "received:" + input.text
