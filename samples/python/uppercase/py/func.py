cd359f8487ee5aab3645a0089695967802e485d0
David Turanski <dturanski@pivotal.io>
2017-12-11 11:54:38 -0500

    Enable GRPC Streaming in Python uppercase sample

--- samples/python/uppercase/py/func.py
+++ samples/python/uppercase/py/func.py
@@ -7,12 +7,17 @@ import function_pb2_grpc as function
 import fntypes_pb2 as types
 from concurrent import futures
 
+'''
+This method’s semantics are a combination of those of the request-streaming method and the response-streaming method. 
+It is passed an iterator of request values and is itself an iterator of response values.
+'''
 class StringFunctionServicer(function.StringFunctionServicer):
 
-    def Call(self, request, context):
-        reply = types.Reply()
-        reply.body = request.body.upper()
-        return reply
+    def Call(self, request_iterator, context):
+        for request in request_iterator:
+            reply = types.Reply()
+            reply.body = request.body.upper()
+            yield reply
 
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
 function.add_StringFunctionServicer_to_server(StringFunctionServicer(), server)
