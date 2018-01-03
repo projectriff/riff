e4270843525ef8e3da0d02dcda9143d8cb360d9f
Thomas Risberg <trisberg@pivotal.io>
2018-01-03 12:50:43 -0500

    Adding request, publish and polishing riff batch files

---
+++ publish.bat
@@ -0,0 +1,68 @@
+@echo off
+:: ##########################################################################
+::  riff demo command for Windows to publish messages
+:: ##########################################################################
+
+:: set local scope for the variables with windows NT shell
+if [%OS%]==[Windows_NT] setlocal
+
+call :capture_instances svc "http-gateway"
+if "%svc%"=="[]" (
+  echo Unable to locate the http-gateway
+  exit /B 1
+)
+
+call :capture_jsonpath svc_type "http-gateway" "{.items[0].spec.type}"
+if [%svc_type%]==[NodePort] (
+  call :capture_cmd address "minikube ip"
+  call :capture_jsonpath port "http-gateway" "{.items[0].spec.ports[?(@.name == 'http')].nodePort}"
+) else (
+  call :capture_jsonpath address "http-gateway" "{.items[0].status.loadBalancer.ingress[0].ip}"
+  if [%address%]==[] (
+    echo External IP is not yet available, try in a few ...
+    exit /B 1
+  )
+  call :capture_kubectl_jsonpath_cmd port "http-gateway" "{.items[0].spec.ports[?(@.name == 'http')].port}"
+)
+
+if [%3]==[] (
+    set count=1
+) else (
+    set count=%3
+)
+if [%4]==[] (
+    set pause=0
+) else (
+    set pause=%4
+)
+
+for /L %%i IN (1,1,%count%) do call :do_post_content %~1 %~2 %%i
+echo.
+
+exit /B %ERRORLEVEL%
+
+:do_post_content
+curl -H "Content-Type: text/plain" -X POST http://%address%:%port%/messages/%1 -d "%2"
+timeout /t %pause% /nobreak > NUL
+exit /B %ERRORLEVEL%
+
+:capture_cmd
+for /f "tokens=* usebackq" %%f in (`%~2`) do (
+  set %1=%%f
+)
+exit /B %ERRORLEVEL%
+
+:capture_instances
+set _tmp_file=%tmp%riff-%random%.txt
+kubectl get svc -l component=%~2 -o jsonpath="{.items}" >  %_tmp_file%
+for /f "delims= tokens=1" %%x in (%_tmp_file%) do set %1=%%x
+del %_tmp_file%
+exit /B %ERRORLEVEL%
+
+:capture_jsonpath
+set _tmp_file=%tmp%\riff-%random%.txt
+set _kcmd=kubectl get svc -l component=%~2 -o jsonpath=%3
+%_kcmd% > %_tmp_file%
+for /f "delims= tokens=1" %%x in (%_tmp_file%) do set %1=%%x
+del %_tmp_file%
+exit /B %ERRORLEVEL%
--- riff.bat
+++ riff.bat
@@ -3,22 +3,22 @@
 ::  riff command line interface for Windows
 :: ##########################################################################
 
-Set RIFF_VERSION=0.0.2
+set RIFF_VERSION=0.0.2
 
-:: Set local scope for the variables with windows NT shell
+:: set local scope for the variables with windows NT shell
 if [%OS%]==[Windows_NT] setlocal
 
 :: handle general help
-if [%1]==[] Set _COMMAND=help
-if [%1]==[--help] Set _COMMAND=help
-if [%1]==[-h] Set _COMMAND=help
+if [%1]==[] set _COMMAND=help
+if [%1]==[--help] set _COMMAND=help
+if [%1]==[-h] set _COMMAND=help
 if [%_COMMAND%]==[help] (
-    CALL :print_usage
-    EXIT /B %ERRORLEVEL%
+    call :print_usage
+    exit /B %ERRORLEVEL%
 )
 
 :: grab the command
-Set _COMMAND=%1
+set _COMMAND=%1
 shift
 
 :: handle commands and arguments/options
@@ -34,53 +34,53 @@ echo %_COMMAND% is an invalid command
 echo.
 echo Type "riff --help" to see valid commands
 echo.
-EXIT /B 1
+exit /B 1
 
 :: parse arguments/options
 :arg_loop
-Set key=%1
+set key=%1
 if [%key%]==[] goto end_args
-Set match=false
-if [%1]==[--name] Set key=-n
-if [%1]==[--filename] Set key=-f
-if [%1]==[--version] Set key=-v
-if [%1]==[--container] Set key=-c
-if [%1]==[--tail] Set key=-t
-if [%1]==[--help] Set key=-h
+set match=false
+if [%1]==[--name] set key=-n
+if [%1]==[--filename] set key=-f
+if [%1]==[--version] set key=-v
+if [%1]==[--container] set key=-c
+if [%1]==[--tail] set key=-t
+if [%1]==[--help] set key=-h
 if [%key%]==[-n] (
-    Set match=true
-    Set FUNCTION=%2
+    set match=true
+    set FUNCTION=%2
     shift
 )
 if [%key%]==[-f] (
-    Set match=true
-    Set FNPATH=%2
+    set match=true
+    set FNPATH=%2
     shift
 )
 if [%key%]==[-v] (
-    Set match=true
-    Set VERSION=%2
+    set match=true
+    set VERSION=%2
     shift
 )
 if [%key%]==[-c] (
-    Set match=true
-    Set CONTAINER=%2
+    set match=true
+    set CONTAINER=%2
     shift
 )
 if [%key%]==[-t] (
-    Set match=true
-    Set TAIL=-f
+    set match=true
+    set TAIL=-f
 )
 if [%key%]==[-h] (
-    Set match=true
-    CALL :print_%_COMMAND%_usage
-    EXIT /B 1
+    set match=true
+    call :print_%_COMMAND%_usage
+    exit /B 1
 )
 if [%match%]==[false] (
     echo.
     echo ERROR: Invalid option: [%key%]
-    CALL :print_%_COMMAND%_usage
-    EXIT /B 1
+    call :print_%_COMMAND%_usage
+    exit /B 1
 )
 shift
 goto arg_loop
@@ -96,82 +96,82 @@ if [%CONTAINER%]==[] set CONTAINER=sidecar
 
 :: execute the commands
 if [%_COMMAND%]==[build] (
-    CALL :build
-    EXIT /B %ERRORLEVEL%
+    call :build
+    exit /B %ERRORLEVEL%
 )
 
 if [%_COMMAND%]==[create] (
-    CALL :create
-    EXIT /B %ERRORLEVEL%
+    call :create
+    exit /B %ERRORLEVEL%
 )
 
 if [%_COMMAND%]==[apply] (
-    CALL :apply
-    EXIT /B %ERRORLEVEL%
+    call :apply
+    exit /B %ERRORLEVEL%
 )
 
 if [%_COMMAND%]==[delete] (
-    CALL :delete
-    EXIT /B %ERRORLEVEL%
+    call :delete
+    exit /B %ERRORLEVEL%
 )
 
 if [%_COMMAND%]==[list] (
-    CALL :list
-    EXIT /B %ERRORLEVEL%
+    call :list
+    exit /B %ERRORLEVEL%
 )
 
 if [%_COMMAND%]==[logs] (
-    CALL :logs
-    EXIT /B %ERRORLEVEL%
+    call :logs
+    exit /B %ERRORLEVEL%
 )
 
 if [%_COMMAND%]==[version] (
-    CALL :version
-    EXIT /B %ERRORLEVEL%
+    call :version
+    exit /B %ERRORLEVEL%
 )
 
-EXIT /B 1
+exit /B 1
 
 :: commands
 :build
   echo Building %FUNCTION% %FNPATH% %VERSION%
   docker build -t projectriff/%FUNCTION%:%VERSION% %FNPATH%
-  EXIT /B %ERRORLEVEL%
+  exit /B %ERRORLEVEL%
 
 :create
   echo Creating %FNPATH% resource[s]
   kubectl create -f %FNPATH%
-  EXIT /B %ERRORLEVEL%
+  exit /B %ERRORLEVEL%
 
 :apply
   echo Applying %FNPATH% resource[s]
   kubectl apply -f %FNPATH%
-  EXIT /B %ERRORLEVEL%
+  exit /B %ERRORLEVEL%
 
 :delete
   echo Deleting %FUNCTION% resource[s]
   kubectl delete function %FUNCTION%
-  EXIT /B %ERRORLEVEL%
+  exit /B %ERRORLEVEL%
 
 :list
   echo Listing function resources
   kubectl get functions
-  EXIT /B %ERRORLEVEL%
+  exit /B %ERRORLEVEL%
 
 :logs
   echo Displaying logs for container %CONTAINER% of function %FUNCTION%
-  Set _tmp_file=%tmp%\riff-%random%.txt
-  Set _kcmd=kubectl get pod -l function=%FUNCTION% -o jsonpath="{.items[0].metadata.name}"
+  set _tmp_file=%tmp%\riff-%random%.txt
+  set _kcmd=kubectl get pod -l function=%FUNCTION% -o jsonpath="{.items[0].metadata.name}"
   %_kcmd% > %_tmp_file%
   set /p _pod= < %_tmp_file%
   del %_tmp_file%
   echo.
   kubectl logs %TAIL% -c %CONTAINER% %_pod%
-  EXIT /B %ERRORLEVEL%
+  exit /B %ERRORLEVEL%
 
 :version
   echo riff version %RIFF_VERSION%
-  EXIT /B 0
+  exit /B 0
 
 
 :: help texts
@@ -192,7 +192,7 @@ echo   version      Display the riff version
 echo.
 echo   Use "riff <command> --help" for more information about a given command.
 echo.
-EXIT /B 0
+exit /B 0
 
 :print_build_usage
 echo.
@@ -209,7 +209,7 @@ echo   -n, --name: the name of the function (defaults to the name of the current
 echo   -v, --version: the version of the function (defaults to 0.0.1)
 echo   -f, --filename: filename, directory, or URL for the code or resource (defaults to the current directory)
 echo.
-EXIT /B 0
+exit /B 0
 
 :print_create_usage
 echo.
@@ -223,7 +223,7 @@ echo Options:
 echo.
 echo   -f, --filename: filename, directory, or URL for the resource definition[s] (defaults to the current directory)
 echo.
-EXIT /B 0
+exit /B 0
 
 :print_apply_usage
 echo.
@@ -238,7 +238,7 @@ echo Options:
 echo.
 echo   -f, --filename: filename, directory, or URL for the resource definition[s] (defaults to the current directory)
 echo.
-EXIT /B 0
+exit /B 0
 
 :print_delete_usage
 echo.
@@ -252,7 +252,7 @@ echo Options:
 echo.
 echo   -n, --name: the name of the function (defaults to the name of the current directory)
 echo.
-EXIT /B 0
+exit /B 0
 
 :print_list_usage
 echo.
@@ -262,7 +262,7 @@ echo Usage:
 echo.
 echo   riff list
 echo.
-EXIT /B 0
+exit /B 0
 
 :print_logs_usage
 echo.
@@ -278,7 +278,7 @@ echo   -n, --name: the name of the function (defaults to the name of the current
 echo   -c, --container: the name of the container, usually sidecar or main (defaults to sidecar)
 echo   -t, --tail: tail the logs
 echo.
-EXIT /B 0
+exit /B 0
 
 :print_version_usage
 echo.
@@ -288,4 +288,4 @@ echo Usage:
 echo.
 echo   riff version
 echo.
-EXIT /B 0
+exit /B 0
