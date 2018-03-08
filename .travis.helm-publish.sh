d2be79f7546ab966950241acbfffbabc8776d88f
Scott Andrews <sandrews@pivotal.io>
2018-03-08 15:00:05 -0500

    Publish helm charts via travis
    The charts now have release agnostic versions that are pinned down when
    published to the helm repository. If the riff version does not end with
    "-snapshot" then the current release is marked as the latest.
    
    Helm charts are only published from the master branch after the test
    complete successfully.

---
+++ .travis.helm-publish.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+riff_version=`cat VERSION`
+helm_charts_bucket='riff-charts'
+helm_charts_url="https://${helm_charts_bucket}.storage.googleapis.com/"
+work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/helm-charts/.work"
+
+if [ "$GCLOUD_CLIENT_SECRET" != ""]; then
+  echo $GCLOUD_CLIENT_SECRET | base64 --decode > client-secret.json
+  gcloud auth activate-service-account --key-file client-secret.json
+  rm client-secret.json
+fi
+
+pushd helm-charts
+  helm init --client-only
+
+  mkdir -p $work_dir
+  gsutil cp "gs://$helm_charts_bucket/index.yaml" $work_dir/
+
+  if [[ $riff_version != *-snapshot ]]; then
+    echo "Setting latest version to $riff_version"
+    echo $riff_version > $work_dir/latest_version
+  fi
+
+  sed -E -i '' "s/tag:[ ]*latest/tag: $riff_version/" riff/values.yaml
+  helm package riff --version "$riff_version" --app-version "$riff_version" --destination $work_dir
+  helm repo index $work_dir --url "$helm_charts_url" --merge $work_dir/index.yaml
+
+  gsutil cp $work_dir/* "gs://$helm_charts_bucket"
+popd
