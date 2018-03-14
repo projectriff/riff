b1106aaf5dfc5ac79ee23d7c6d608a9538f82b76
trisberg <trisberg@pivotal.io>
2018-03-14 17:11:33 -0400

    Fix the issue with empty handler for Java

--- riff-cli/cmd/init.go
+++ riff-cli/cmd/init.go
@@ -82,7 +82,7 @@ func InitJava(initOptions *options.InitOptions) (*cobra.Command, *options.InitOp
 		Short: "Initialize a Java function",
 		Long:  utils.InitJavaCmdLong(),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			//initOptions.Handler,_ = cmd.Flags().GetString("handler")
+			initOptions.Handler,_ = cmd.Flags().GetString("handler")
 			err := initializers.Java().Initialize(*initOptions)
 			if err != nil {
 				return err
