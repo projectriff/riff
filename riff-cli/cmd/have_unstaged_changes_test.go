a0e1f901f9237a9ecbfeb7e566bc0452fcc9bb17
trisberg <trisberg@pivotal.io>
2018-03-29 13:08:59 -0400

    Increase context timeout for git command to 5s

--- riff-cli/cmd/have_unstaged_changes_test.go
+++ riff-cli/cmd/have_unstaged_changes_test.go
@@ -20,7 +20,7 @@ type unstagedChangesMatcher struct{}
 
 func (matcher *unstagedChangesMatcher) Match(actual interface{}) (success bool, err error) {
 	// Create a new context and add a timeout to it
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel() // The cancel should be deferred so resources are cleaned up
 
 	// Create the command with our context
