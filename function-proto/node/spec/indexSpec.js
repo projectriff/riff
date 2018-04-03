8641fe2c69e6376eddb05b6f429c5760cd6dc0c0
Scott Andrews <sandrews@pivotal.io>
2018-04-03 13:27:04 -0400

    Cleanup tests as well

--- function-proto/node/spec/indexSpec.js
+++ function-proto/node/spec/indexSpec.js
@@ -1,135 +1,17 @@
 describe('function-proto', () => {
-    const { FunctionInvokerService, FunctionInvokerClient, MessageBuilder, MessageHeaders } = require('..');
+    const { FunctionInvokerService, FunctionInvokerClient } = require('..');
     const grpc = require('grpc');
     const port = 50051;
 
-    describe('MessageHeaders', () => {
-        it('parses a plain JS object', () => {
-            const obj = {
-                'Content-Type': {
-                    values: ['application/json']
-                }
-            }
-            const headers = MessageHeaders.fromObject(obj);
-            expect(headers.getValue('content-type')).toBe('application/json')
-        });
-
-        it('creates a plain JS object', () => {
-            const headers = new MessageHeaders()
-                .addHeader('Content-Type', 'application/json');
-            expect(headers.toObject()).toEqual({
-                'Content-Type': {
-                    values: ['application/json']
-                }
-            });
-        });
-
-        it('creates a plain JS object', () => {
-            const headers = new MessageHeaders()
-                .addHeader('Content-Type', 'application/json');
-            expect(headers.toObject()).toEqual({
-                'Content-Type': {
-                    values: ['application/json']
-                }
-            });
-        });
-
-        it('allows for multiple values', () => {
-            const headers = new MessageHeaders()
-                .addHeader('Accept', '*/*;q=0.1', 'text/plain;q=0.9');
-            expect(headers.getValues('accept')).toEqual(['*/*;q=0.1', 'text/plain;q=0.9']);
-        });
-
-        it('returns the first value for a header', () => {
-            const headers = new MessageHeaders()
-                .addHeader('Accept', '*/*;q=0.1', 'text/plain;q=0.9');
-            expect(headers.getValue('accept')).toEqual('*/*;q=0.1');
-        });
-
-        it('conflates header names to be case insensite', () => {
-            const headers = new MessageHeaders()
-                .addHeader('Accept', '*/*;q=0.1')
-                .addHeader('accept', 'text/plain;q=0.9');
-            expect(headers.toObject()).toEqual({
-                'Accept': {
-                    values: ['*/*;q=0.1', 'text/plain;q=0.9']
-                }
-            });
-        });
-
-        it('converts header values to strings', () => {
-            const headers = new MessageHeaders()
-                .addHeader('correlationId', 1234);
-            expect(headers.toObject()).toEqual({
-                'correlationId': {
-                    values: ['1234']
-                }
-            });
-        });
-
-        it('handles multiple headers', () => {
-            const headers = new MessageHeaders()
-                .addHeader('Accept', '*/*;q=0.1')
-                .addHeader('accept', 'text/plain;q=0.9')
-                .addHeader('Content-Type', 'application/json');
-            expect(headers.toObject()).toEqual({
-                'Accept': {
-                    values: ['*/*;q=0.1', 'text/plain;q=0.9']
-                },
-                'Content-Type': {
-                    values: ['application/json']
-                }
-            });
-        });
-
-        it('is immutable', () => {
-            const empty = new MessageHeaders();
-            const one = empty.addHeader('correlationId', '1234');
-            expect(one).not.toBe(empty);
-            expect(empty.toObject()).toEqual({});
-            expect(one.toObject()).toEqual({
-                correlationId: {
-                    values: ['1234']
-                }
-            });
-        });
-    });
-
-    it('builds a message', () => {
-        const theMessage = new MessageBuilder()
-            .addHeader('Header-Name', 'headerValue 1')
-            .addHeader('Header-Name', 'headerValue 2')
-            .payload('will be replaced')
-            .payload('riff')
-            .build();
-
-        expect(theMessage).toEqual({
+    it('generates a grpc client and server', done => {
+        const theMessage = {
             headers: {
                 'Header-Name': {
-                    values: [
-                        'headerValue 1',
-                        'headerValue 2'
-                    ]
+                    values: ['headerValue']
                 }
             },
             payload: Buffer.from('riff')
-        });
-    });
-
-    it('builds an empty message', () => {
-        const theMessage = new MessageBuilder().build();
-
-        expect(theMessage).toEqual({
-            headers: {},
-            payload: Buffer.from([])
-        });
-    });
-
-    it('generates a grpc client and server', done => {
-        const theMessage = new MessageBuilder()
-            .addHeader('Header-Name', 'headerValue')
-            .payload('riff')
-            .build();
+        };
 
         const server = new grpc.Server();
         server.addService(FunctionInvokerService, {
