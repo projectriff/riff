ecd16df6b3ef3d617c5469d20757d7709a4e8c89
Scott Andrews <sandrews@pivotal.io>
2018-09-17 16:32:06 -0400

    Revert "Switch to go modules (#783)"
    This reverts commit 8110bbee7be9490da2cf621d8c3bb26aeef5ffd3.

---
+++ vendor/github.com/boz/kcache/types/replicationcontroller/client.go
@@ -0,0 +1,14 @@
+package replicationcontroller
+
+import (
+	"github.com/boz/kcache/client"
+	"k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+const resourceName = string(v1.ResourceReplicationControllers)
+
+func NewClient(cs kubernetes.Interface, ns string) client.Client {
+	scope := cs.CoreV1()
+	return client.ForResource(scope.RESTClient(), resourceName, ns)
+}
