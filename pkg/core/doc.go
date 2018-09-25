86ef9f4a1ade6cb86b8f693dc9fb59b219717c49
Florent Biville <fbiville@pivotal.io>
2018-09-25 01:25:44 -0400

    Add missing license headers and normalize all
    Fixes #839.

--- pkg/core/doc.go
+++ pkg/core/doc.go
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 // package core contains a high level client for interacting with knative primitives used to realise, amongst other
