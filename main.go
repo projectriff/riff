debc56d99cdc778429a088a4ba4c70408368196f
Eric Bottard <ebottard@pivotal.io>
2018-11-21 16:46:12 +0100

    Bump stable to latest builder
    Includes a fix for https://github.com/projectriff/riff-buildpack/issues/30

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ var (
 				"https://storage.googleapis.com/knative-releases/eventing/previous/v20181031-a2f9417/release-clusterbus-stub.yaml",
 			},
 			Namespace: []string{
-				"https://storage.googleapis.com/projectriff/riff-buildtemplate/riff-cnb-buildtemplate-0.0.1-snapshot-ci-9e5b731f8c36.yaml",
+				"https://storage.googleapis.com/projectriff/riff-buildtemplate/riff-cnb-buildtemplate-0.0.1-snapshot-ci-31a4547a8f07.yaml",
 			},
 		},
 	}
