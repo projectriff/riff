18e1c8b5270b4e7096f8522ce8be7fb6db2e00f3
Scott Andrews <scott@andrews.me>
2018-12-19 16:05:49 -0500

    Publish riff cli snapshots (#1032)
    - stage cli release after unit tests pass
    - use staged cli build in fats
    - promote cli release after fats passes
    
    Fixes #1031

---
+++ .travis/publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+version=`cat VERSION`
+commit=$(git rev-parse HEAD)
+
+gcloud auth activate-service-account --key-file <(echo $GCLOUD_CLIENT_SECRET | base64 --decode)
+
+bucket=gs://projectriff/riff-cli/releases
+
+gsutil rsync -d ${bucket}/builds/v${version}-${commit} ${bucket}/v${version}
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  gsutil rsync -d ${bucket}/builds/v${version}-${commit} ${bucket}/latest
+fi
