b539ceee96de91dab2c20551c1fe1e1561770f50
Scott Andrews <sandrews@pivotal.io>
2019-01-22 12:31:54 -0500

    add missing fold end for system status

--- .travis/fats-cleanup.sh
+++ .travis/fats-cleanup.sh
@@ -25,6 +25,7 @@ if [ -d "$fats_dir" ]; then
     travis_fold start system-status
     echo "System status"
     kubectl get deployments,services,pods --all-namespaces || true
+    travis_fold end system-status
   fi
 
   travis_fold start system-uninstall
