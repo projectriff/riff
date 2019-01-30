61deeb7a4cf5b07ca236ccbb15789e55b38e062a
Florent Biville <fbiville@pivotal.io>
2019-01-30 15:24:05 +0100

    Rename parameters

--- cmd/commands/parallel.go
+++ cmd/commands/parallel.go
@@ -2,17 +2,17 @@ package commands
 
 import "sync"
 
-func ApplyInParallel(names []string, deleteResource func(string) error) []error {
+func ApplyInParallel(names []string, action func(string) error) []error {
 	count := len(names)
-	deleteErrors := make([]error, count)
+	result := make([]error, count)
 	waitGroup := sync.WaitGroup{}
 	waitGroup.Add(count)
 	for i, name := range names {
 		go func(i int, name string) {
-			deleteErrors[i] = deleteResource(name)
+			result[i] = action(name)
 			waitGroup.Done()
 		}(i, name)
 	}
 	waitGroup.Wait()
-	return deleteErrors
+	return result
 }
