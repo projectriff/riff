999bcb86412b676430457886bb2a373e03b308b1
Scott Andrews <scott@andrews.me>
2019-03-06 12:56:00 -0500

    Provide a cache volume for builds (#1135)
    The build template now defaults the cache to an empty dir, individual
    builds may define their own volume and define the CACHE argument to use
    that volume.
    
    This behavior makes the build template more useable, since it no longer
    requires as PVC, and preserves the current cache behavior.

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ import (
 
 var (
 	// TODO update to a release version before releasing riff
-	builderVersion  = "0.2.0-snapshot-ci-1aa01f4f464c"
+	builderVersion  = "0.2.0-snapshot-ci-a974b8e885d3"
 	builder         = fmt.Sprintf("projectriff/builder:%s", builderVersion)
 	defaultRunImage = "packs/run:v3alpha2"
 
