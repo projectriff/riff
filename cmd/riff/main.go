3aff8cef1da1fd72f45a4d1900c17428b94e40a9
trisberg <trisberg@pivotal.io>
2019-03-25 13:42:28 -0400

    Update Knative serving to latest 0.4.1 release

--- cmd/riff/main.go
+++ cmd/riff/main.go
@@ -35,7 +35,7 @@ var (
 			},
 			Knative: []string{
 				"https://storage.googleapis.com/knative-releases/build/previous/v0.4.0/build.yaml",
-				"https://storage.googleapis.com/knative-releases/serving/previous/v0.4.0/serving.yaml",
+				"https://storage.googleapis.com/knative-releases/serving/previous/v0.4.1/serving.yaml",
 				"https://raw.githubusercontent.com/knative/serving/v0.4.0/third_party/config/build/clusterrole.yaml",
 				"https://storage.googleapis.com/knative-releases/eventing/previous/v0.4.0/eventing.yaml",
 				"https://storage.googleapis.com/knative-releases/eventing/previous/v0.4.0/in-memory-channel.yaml",
