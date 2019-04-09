fb1a3162bd5f991ddb04ea37ada13217d2217382
trisberg <trisberg@pivotal.io>
2019-04-09 11:31:40 -0400

    update to knative serving v0.5.1

--- cmd/riff/main.go
+++ cmd/riff/main.go
@@ -35,8 +35,8 @@ var (
 			},
 			Knative: []string{
 				"https://storage.googleapis.com/knative-releases/build/previous/v0.5.0/build.yaml",
-				"https://storage.googleapis.com/knative-releases/serving/previous/v0.5.0/serving.yaml",
-				"https://raw.githubusercontent.com/knative/serving/v0.5.0/third_party/config/build/clusterrole.yaml",
+				"https://storage.googleapis.com/knative-releases/serving/previous/v0.5.1/serving.yaml",
+				"https://raw.githubusercontent.com/knative/serving/v0.5.1/third_party/config/build/clusterrole.yaml",
 				"https://storage.googleapis.com/knative-releases/eventing/previous/v0.4.0/eventing.yaml",
 				"https://storage.googleapis.com/knative-releases/eventing/previous/v0.4.0/in-memory-channel.yaml",
 				// TODO update to a release version before releasing riff
