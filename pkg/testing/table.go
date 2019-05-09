9d91f4e086fe98aa3d507e11c4eca18f5191fb5c
Scott Andrews <sandrews@pivotal.io>
2019-05-09 11:08:26 -0400

    Allow running a single table record in isolation or skipping a record in the table

--- pkg/testing/table.go
+++ pkg/testing/table.go
@@ -34,10 +34,11 @@ import (
 	clientgotesting "k8s.io/client-go/testing"
 )
 
-type CommandTable []CommandTableRow
+type CommandTable []CommandTableRecord
 
-type CommandTableRow struct {
+type CommandTableRecord struct {
 	Name string
+	Skip bool
 
 	// environment
 	Config       *riff.Config
@@ -65,158 +66,166 @@ type CommandTableRow struct {
 func (ct CommandTable) Run(t *T, cmdFactory func(*riff.Config) *cobra.Command) {
 	for _, ctr := range ct {
 		t.Run(ctr.Name, func(t *T) {
-			c := ctr.Config
-			if c == nil {
-				c = &riff.Config{}
-			}
-			client := NewClient(ctr.GivenObjects...)
-			c.Client = client
-
-			if ctr.Prepare != nil {
-				if err := ctr.Prepare(c); err != nil {
-					t.Errorf("error during prepare: %s", err)
-				}
-			}
-
-			// Validate all objects that implement Validatable
-			client.PrependReactor("create", "*", func(action clientgotesting.Action) (handled bool, ret runtime.Object, err error) {
-				return kntesting.ValidateCreates(context.Background(), action)
-			})
-			client.PrependReactor("update", "*", func(action clientgotesting.Action) (handled bool, ret runtime.Object, err error) {
-				return kntesting.ValidateUpdates(context.Background(), action)
-			})
-
-			for i := range ctr.WithReactors {
-				// in reverse order since we prepend
-				reactor := ctr.WithReactors[len(ctr.WithReactors)-1-i]
-				client.PrependReactor("*", "*", reactor)
-			}
-
-			cmd := cmdFactory(c)
-			output := &bytes.Buffer{}
-
-			cmd.SetArgs(ctr.Args)
-			cmd.SetOutput(output)
-
-			err := cmd.Execute()
-
-			if expected, actual := ctr.ShouldError, err != nil; expected != actual {
-				if expected {
-					t.Errorf("expected command to error, actual %v", err)
-				} else {
-					t.Errorf("expected command not to error, actual %q", err)
-				}
-			}
-
-			actions, err := client.ActionRecorderList.ActionsByVerb()
-			if err != nil {
-				t.Errorf("Error capturing actions by verb: %q", err)
-			}
-
-			// Previous state is used to diff resource expected state for update requests that were missed.
-			objPrevState := map[string]runtime.Object{}
-			for _, o := range ctr.GivenObjects {
-				objPrevState[objKey(o)] = o
-			}
-
-			for i, expected := range ctr.ExpectCreates {
-				if i >= len(actions.Creates) {
-					t.Errorf("Missing create: %#v", expected)
-					continue
-				}
-				actual := actions.Creates[i]
-				obj := actual.GetObject()
-				objPrevState[objKey(obj)] = obj
-
-				if at, et := reflect.TypeOf(obj).String(), reflect.TypeOf(expected).String(); at != et {
-					t.Errorf("Unexpected create expected type %q, actually %q", et, at)
-				} else if diff := cmp.Diff(expected, obj, ignoreLastTransitionTime, safeDeployDiff, cmpopts.EquateEmpty()); diff != "" {
-					t.Errorf("Unexpected create (-expected, +actual): %s", diff)
-				}
-			}
-			if actual, expected := len(actions.Creates), len(ctr.ExpectCreates); actual > expected {
-				for _, extra := range actions.Creates[expected:] {
-					t.Errorf("Extra create: %#v", extra)
-				}
-			}
-
-			for i, expected := range ctr.ExpectUpdates {
-				if i >= len(actions.Updates) {
-					key := objKey(expected)
-					oldObj, ok := objPrevState[key]
-					if !ok {
-						t.Errorf("Object %s was never created: expected: %#v", key, expected)
-						continue
-					}
-					t.Errorf("Missing update for %s (-expected, +prevState): %s", key,
-						cmp.Diff(expected, oldObj, ignoreLastTransitionTime, safeDeployDiff, cmpopts.EquateEmpty()))
-					continue
-				}
-
-				actual := actions.Updates[i]
-				obj := actual.GetObject()
-
-				if actual.GetSubresource() != "" {
-					t.Errorf("Update was invalid - it should not include a subresource: %#v", actual)
-				}
-
-				// Update the object state.
-				objPrevState[objKey(obj)] = obj
-
-				if at, et := reflect.TypeOf(obj).String(), reflect.TypeOf(expected).String(); at != et {
-					t.Errorf("Unexpected update expected type %q, actually %q", et, at)
-				} else if diff := cmp.Diff(expected, obj, ignoreLastTransitionTime, safeDeployDiff, cmpopts.EquateEmpty()); diff != "" {
-					t.Errorf("Unexpected update (-expected, +actual): %s", diff)
-				}
-			}
-
-			if actual, expected := len(actions.Updates), len(ctr.ExpectUpdates); actual > expected {
-				for _, extra := range actions.Updates[expected:] {
-					t.Errorf("Extra update: %#v", extra)
-				}
-			}
-			for i, expected := range ctr.ExpectDeletes {
-				if i >= len(actions.Deletes) {
-					t.Errorf("Missing delete: %#v", expected)
-					continue
-				}
-				actual := NewDeleteRef(actions.Deletes[i])
-				if diff := cmp.Diff(expected, actual); diff != "" {
-					t.Errorf("Unexpected delete (-expected, +actual): %s", diff)
-				}
-			}
-			if actual, expected := len(actions.Deletes), len(ctr.ExpectDeletes); actual > expected {
-				for _, extra := range actions.Deletes[expected:] {
-					t.Errorf("Extra delete: %#v", extra)
-				}
-			}
-
-			for i, expected := range ctr.ExpectDeleteCollections {
-				if i >= len(actions.DeleteCollections) {
-					t.Errorf("Missing delete-collection: %#v", expected)
-					continue
-				}
-				actual := NewDeleteCollectionRef(actions.DeleteCollections[i])
-				if diff := cmp.Diff(expected, actual); diff != "" {
-					t.Errorf("Unexpected delete collection (-expected, +actual): %s", diff)
-				}
-			}
-			if actual, expected := len(actions.DeleteCollections), len(ctr.ExpectDeleteCollections); actual > expected {
-				for _, extra := range actions.DeleteCollections[expected:] {
-					t.Errorf("Extra delete-collection: %#v", extra)
-				}
-			}
-
-			if ctr.Verify != nil {
-				ctr.Verify(t, output.String(), err)
-			}
-
-			if ctr.Cleanup != nil {
-				if err := ctr.Cleanup(c); err != nil {
-					t.Errorf("error during cleanup: %s", err)
-				}
-			}
+			ctr.Run(t, cmdFactory)
 		})
+	}
+}
+
+func (ctr CommandTableRecord) Run(t *T, cmdFactory func(*riff.Config) *cobra.Command) {
+	if ctr.Skip {
+		t.SkipNow()
+	}
+
+	c := ctr.Config
+	if c == nil {
+		c = &riff.Config{}
+	}
+	client := NewClient(ctr.GivenObjects...)
+	c.Client = client
+
+	if ctr.Prepare != nil {
+		if err := ctr.Prepare(c); err != nil {
+			t.Errorf("error during prepare: %s", err)
+		}
+	}
+
+	// Validate all objects that implement Validatable
+	client.PrependReactor("create", "*", func(action clientgotesting.Action) (handled bool, ret runtime.Object, err error) {
+		return kntesting.ValidateCreates(context.Background(), action)
+	})
+	client.PrependReactor("update", "*", func(action clientgotesting.Action) (handled bool, ret runtime.Object, err error) {
+		return kntesting.ValidateUpdates(context.Background(), action)
+	})
+
+	for i := range ctr.WithReactors {
+		// in reverse order since we prepend
+		reactor := ctr.WithReactors[len(ctr.WithReactors)-1-i]
+		client.PrependReactor("*", "*", reactor)
+	}
+
+	cmd := cmdFactory(c)
+	output := &bytes.Buffer{}
+
+	cmd.SetArgs(ctr.Args)
+	cmd.SetOutput(output)
+
+	err := cmd.Execute()
+
+	if expected, actual := ctr.ShouldError, err != nil; expected != actual {
+		if expected {
+			t.Errorf("expected command to error, actual %v", err)
+		} else {
+			t.Errorf("expected command not to error, actual %q", err)
+		}
+	}
+
+	actions, err := client.ActionRecorderList.ActionsByVerb()
+	if err != nil {
+		t.Errorf("Error capturing actions by verb: %q", err)
+	}
+
+	// Previous state is used to diff resource expected state for update requests that were missed.
+	objPrevState := map[string]runtime.Object{}
+	for _, o := range ctr.GivenObjects {
+		objPrevState[objKey(o)] = o
+	}
+
+	for i, expected := range ctr.ExpectCreates {
+		if i >= len(actions.Creates) {
+			t.Errorf("Missing create: %#v", expected)
+			continue
+		}
+		actual := actions.Creates[i]
+		obj := actual.GetObject()
+		objPrevState[objKey(obj)] = obj
+
+		if at, et := reflect.TypeOf(obj).String(), reflect.TypeOf(expected).String(); at != et {
+			t.Errorf("Unexpected create expected type %q, actually %q", et, at)
+		} else if diff := cmp.Diff(expected, obj, ignoreLastTransitionTime, safeDeployDiff, cmpopts.EquateEmpty()); diff != "" {
+			t.Errorf("Unexpected create (-expected, +actual): %s", diff)
+		}
+	}
+	if actual, expected := len(actions.Creates), len(ctr.ExpectCreates); actual > expected {
+		for _, extra := range actions.Creates[expected:] {
+			t.Errorf("Extra create: %#v", extra)
+		}
+	}
+
+	for i, expected := range ctr.ExpectUpdates {
+		if i >= len(actions.Updates) {
+			key := objKey(expected)
+			oldObj, ok := objPrevState[key]
+			if !ok {
+				t.Errorf("Object %s was never created: expected: %#v", key, expected)
+				continue
+			}
+			t.Errorf("Missing update for %s (-expected, +prevState): %s", key,
+				cmp.Diff(expected, oldObj, ignoreLastTransitionTime, safeDeployDiff, cmpopts.EquateEmpty()))
+			continue
+		}
+
+		actual := actions.Updates[i]
+		obj := actual.GetObject()
+
+		if actual.GetSubresource() != "" {
+			t.Errorf("Update was invalid - it should not include a subresource: %#v", actual)
+		}
+
+		// Update the object state.
+		objPrevState[objKey(obj)] = obj
+
+		if at, et := reflect.TypeOf(obj).String(), reflect.TypeOf(expected).String(); at != et {
+			t.Errorf("Unexpected update expected type %q, actually %q", et, at)
+		} else if diff := cmp.Diff(expected, obj, ignoreLastTransitionTime, safeDeployDiff, cmpopts.EquateEmpty()); diff != "" {
+			t.Errorf("Unexpected update (-expected, +actual): %s", diff)
+		}
+	}
+
+	if actual, expected := len(actions.Updates), len(ctr.ExpectUpdates); actual > expected {
+		for _, extra := range actions.Updates[expected:] {
+			t.Errorf("Extra update: %#v", extra)
+		}
+	}
+	for i, expected := range ctr.ExpectDeletes {
+		if i >= len(actions.Deletes) {
+			t.Errorf("Missing delete: %#v", expected)
+			continue
+		}
+		actual := NewDeleteRef(actions.Deletes[i])
+		if diff := cmp.Diff(expected, actual); diff != "" {
+			t.Errorf("Unexpected delete (-expected, +actual): %s", diff)
+		}
+	}
+	if actual, expected := len(actions.Deletes), len(ctr.ExpectDeletes); actual > expected {
+		for _, extra := range actions.Deletes[expected:] {
+			t.Errorf("Extra delete: %#v", extra)
+		}
+	}
+
+	for i, expected := range ctr.ExpectDeleteCollections {
+		if i >= len(actions.DeleteCollections) {
+			t.Errorf("Missing delete-collection: %#v", expected)
+			continue
+		}
+		actual := NewDeleteCollectionRef(actions.DeleteCollections[i])
+		if diff := cmp.Diff(expected, actual); diff != "" {
+			t.Errorf("Unexpected delete collection (-expected, +actual): %s", diff)
+		}
+	}
+	if actual, expected := len(actions.DeleteCollections), len(ctr.ExpectDeleteCollections); actual > expected {
+		for _, extra := range actions.DeleteCollections[expected:] {
+			t.Errorf("Extra delete-collection: %#v", extra)
+		}
+	}
+
+	if ctr.Verify != nil {
+		ctr.Verify(t, output.String(), err)
+	}
+
+	if ctr.Cleanup != nil {
+		if err := ctr.Cleanup(c); err != nil {
+			t.Errorf("error during cleanup: %s", err)
+		}
 	}
 }
 
