0197838691abb1d1019bdbccd701f391705e603d
Mark Fisher <markfisher@users.noreply.github.com>
2019-05-21 15:53:36 -0400

    add processor list, create, and delete (#1288)

---
+++ pkg/riff/commands/processor_create_test.go
@@ -0,0 +1,207 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package commands_test
+
+import (
+	"testing"
+
+	"github.com/projectriff/riff/pkg/cli"
+	"github.com/projectriff/riff/pkg/riff/commands"
+	rifftesting "github.com/projectriff/riff/pkg/testing"
+	streamv1alpha1 "github.com/projectriff/system/pkg/apis/stream/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func TestProcessorCreateOptions(t *testing.T) {
+	table := rifftesting.OptionsTable{
+		{
+			Name: "invalid resource",
+			Options: &commands.ProcessorCreateOptions{
+				ResourceOptions: rifftesting.InvalidResourceOptions,
+			},
+			ExpectFieldError: rifftesting.InvalidResourceOptionsFieldError.Also(
+				cli.ErrMissingField(cli.FunctionRefFlagName),
+				cli.ErrMissingField(cli.InputFlagName),
+			),
+		},
+		{
+			Name: "with inputs but no outputs",
+			Options: &commands.ProcessorCreateOptions{
+				ResourceOptions: rifftesting.ValidResourceOptions,
+				FunctionRef:     "my-function",
+				Inputs:          []string{"input1", "input2"},
+			},
+			ShouldValidate: true,
+		},
+		{
+			Name: "with inputs and outputs",
+			Options: &commands.ProcessorCreateOptions{
+				ResourceOptions: rifftesting.ValidResourceOptions,
+				FunctionRef:     "my-function",
+				Inputs:          []string{"input1", "input2"},
+				Outputs:         []string{"output1", "output2"},
+			},
+			ShouldValidate: true,
+		},
+	}
+
+	table.Run(t)
+}
+
+func TestProcessorCreateCommand(t *testing.T) {
+	defaultNamespace := "default"
+	processorName := "my-processor"
+	functionRef := "my-func"
+	inputName := "input"
+	outputName := "output"
+	inputNameOther := "otherinput"
+	outputNameOther := "otheroutput"
+
+	table := rifftesting.CommandTable{
+		{
+			Name:        "invalid args",
+			Args:        []string{},
+			ShouldError: true,
+		},
+		{
+			Name: "create with single input",
+			Args: []string{processorName, cli.FunctionRefFlagName, functionRef, cli.InputFlagName, inputName},
+			ExpectCreates: []runtime.Object{
+				&streamv1alpha1.Processor{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: defaultNamespace,
+						Name:      processorName,
+					},
+					Spec: streamv1alpha1.ProcessorSpec{
+						FunctionRef: functionRef,
+						Inputs:      []string{inputName},
+					},
+				},
+			},
+			ExpectOutput: `
+Created processor "my-processor"
+`,
+		},
+		{
+			Name: "create with multiple inputs",
+			Args: []string{processorName, cli.FunctionRefFlagName, functionRef, cli.InputFlagName, inputName, cli.InputFlagName, inputNameOther},
+			ExpectCreates: []runtime.Object{
+				&streamv1alpha1.Processor{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: defaultNamespace,
+						Name:      processorName,
+					},
+					Spec: streamv1alpha1.ProcessorSpec{
+						FunctionRef: functionRef,
+						Inputs:      []string{inputName, inputNameOther},
+					},
+				},
+			},
+			ExpectOutput: `
+Created processor "my-processor"
+`,
+		},
+		{
+			Name: "create with single output",
+			Args: []string{processorName, cli.FunctionRefFlagName, functionRef, cli.InputFlagName, inputName, cli.InputFlagName, inputNameOther, cli.OutputFlagName, outputName},
+			ExpectCreates: []runtime.Object{
+				&streamv1alpha1.Processor{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: defaultNamespace,
+						Name:      processorName,
+					},
+					Spec: streamv1alpha1.ProcessorSpec{
+						FunctionRef: functionRef,
+						Inputs:      []string{inputName, inputNameOther},
+						Outputs:     []string{outputName},
+					},
+				},
+			},
+			ExpectOutput: `
+Created processor "my-processor"
+`,
+		},
+		{
+			Name: "create with multiple outputs",
+			Args: []string{processorName, cli.FunctionRefFlagName, functionRef, cli.InputFlagName, inputName, cli.InputFlagName, inputNameOther, cli.OutputFlagName, outputName, cli.OutputFlagName, outputNameOther},
+			ExpectCreates: []runtime.Object{
+				&streamv1alpha1.Processor{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: defaultNamespace,
+						Name:      processorName,
+					},
+					Spec: streamv1alpha1.ProcessorSpec{
+						FunctionRef: functionRef,
+						Inputs:      []string{inputName, inputNameOther},
+						Outputs:     []string{outputName, outputNameOther},
+					},
+				},
+			},
+			ExpectOutput: `
+Created processor "my-processor"
+`,
+		},
+		{
+			Name: "error existing processor",
+			Args: []string{processorName, cli.FunctionRefFlagName, functionRef, cli.InputFlagName, inputName},
+			GivenObjects: []runtime.Object{
+				&streamv1alpha1.Processor{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: defaultNamespace,
+						Name:      processorName,
+					},
+				},
+			},
+			ExpectCreates: []runtime.Object{
+				&streamv1alpha1.Processor{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: defaultNamespace,
+						Name:      processorName,
+					},
+					Spec: streamv1alpha1.ProcessorSpec{
+						FunctionRef: functionRef,
+						Inputs:      []string{inputName},
+					},
+				},
+			},
+			ShouldError: true,
+		},
+		{
+			Name: "error during create",
+			Args: []string{processorName, cli.FunctionRefFlagName, functionRef, cli.InputFlagName, inputName},
+			WithReactors: []rifftesting.ReactionFunc{
+				rifftesting.InduceFailure("create", "processors"),
+			},
+			ExpectCreates: []runtime.Object{
+				&streamv1alpha1.Processor{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: defaultNamespace,
+						Name:      processorName,
+					},
+					Spec: streamv1alpha1.ProcessorSpec{
+						FunctionRef: functionRef,
+						Inputs:      []string{inputName},
+					},
+				},
+			},
+			ShouldError: true,
+		},
+	}
+
+	table.Run(t, commands.NewProcessorCreateCommand)
+}
--- pkg/riff/commands/processor_delete.go
+++ pkg/riff/commands/processor_delete.go
@@ -18,23 +18,43 @@ package commands
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/projectriff/riff/pkg/cli"
 	"github.com/spf13/cobra"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type ProcessorDeleteOptions struct {
-	Namespace string
+	cli.DeleteOptions
 }
 
 func (opts *ProcessorDeleteOptions) Validate(ctx context.Context) *cli.FieldError {
-	// TODO implement
-	return nil
+	errs := &cli.FieldError{}
+
+	errs = errs.Also(opts.DeleteOptions.Validate(ctx))
+
+	return errs
 }
 
 func (opts *ProcessorDeleteOptions) Exec(ctx context.Context, c *cli.Config) error {
-	return fmt.Errorf("not implemented")
+	client := c.Stream().Processors(opts.Namespace)
+
+	if opts.All {
+		if err := client.DeleteCollection(nil, metav1.ListOptions{}); err != nil {
+			return err
+		}
+		c.Successf("Deleted processors in namespace %q\n", opts.Namespace)
+		return nil
+	}
+
+	for _, name := range opts.Names {
+		if err := client.Delete(name, nil); err != nil {
+			return err
+		}
+		c.Successf("Deleted processor %q\n", name)
+	}
+
+	return nil
 }
 
 func NewProcessorDeleteCommand(c *cli.Config) *cobra.Command {
@@ -44,12 +64,15 @@ func NewProcessorDeleteCommand(c *cli.Config) *cobra.Command {
 		Use:     "delete",
 		Short:   "<todo>",
 		Example: "<todo>",
-		Args:    cli.Args(),
+		Args: cli.Args(
+			cli.NamesArg(&opts.Names),
+		),
 		PreRunE: cli.ValidateOptions(opts),
 		RunE:    cli.ExecOptions(c, opts),
 	}
 
 	cli.NamespaceFlag(cmd, c, &opts.Namespace)
+	cmd.Flags().BoolVar(&opts.All, cli.StripDash(cli.AllFlagName), false, "<todo>")
 
 	return cmd
 }
