e71ab4647c175c8559cb5fa6bfd6236e4bf4bc7a
Scott Andrews <sandrews@pivotal.io>
2019-06-14 17:52:54 -0400

    Update FATS (#1330)
    Removing references to Travis

--- ci/fats.sh
+++ ci/fats.sh
@@ -10,16 +10,12 @@ commit=$(git rev-parse HEAD)
 # fetch FATS scripts
 fats_dir=`dirname "${BASH_SOURCE[0]}"`/fats
 fats_repo="projectriff/fats"
-fats_refspec=c6dfc9a6ce1883f0db71a5d155c6a29702273265 # projectriff/fats master as of 2019-06-05
+fats_refspec=5c1377d800a17bdd5ef11648ef3e9014997b85d3 # projectriff/fats master as of 2019-06-14
 source `dirname "${BASH_SOURCE[0]}"`/fats-fetch.sh $fats_dir $fats_refspec $fats_repo
 source $fats_dir/.util.sh
 
-$fats_dir/install.sh kubectl
-$fats_dir/install.sh kail
-$fats_dir/install.sh duffle
 
 # install riff-cli
-travis_fold start install-riff
 echo "Installing riff"
 if [ "$machine" == "MinGw" ]; then
   curl https://storage.googleapis.com/projectriff/riff-cli/releases/builds/v${version}-${commit}/riff-windows-amd64.zip > riff.zip
@@ -30,14 +26,13 @@ else
   chmod +x riff
   sudo cp riff /usr/bin/riff
 fi
-travis_fold end install-riff
 
 # start FATS
 source $fats_dir/start.sh
 
 # install riff system
-travis_fold start system-install
 echo "Installing riff system"
+$fats_dir/install.sh duffle
 duffle credentials add `dirname "${BASH_SOURCE[0]}"`/duffle-creds/k8s.yaml
 curl -O https://storage.googleapis.com/projectriff/riff-cnab/snapshots/riff-bundle-latest.json
 duffle install riff riff-bundle-latest.json --bundle-is-file --credentials k8s --insecure
@@ -49,7 +44,6 @@ wait_for_ingress_ready 'istio-ingressgateway' 'istio-system'
 # setup namespace
 kubectl create namespace $NAMESPACE
 fats_create_push_credentials $NAMESPACE
-travis_fold end system-install
 
 # run test functions
 source $fats_dir/functions/helpers.sh
