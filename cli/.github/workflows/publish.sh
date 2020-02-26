35782431a25ad0431a76b2c1e03bb0363692df16
Scott Andrews <sandrews@pivotal.io>
2020-02-25 22:23:39 -0500

    centralize VERSION

--- cli/.github/workflows/publish.sh
+++ cli/.github/workflows/publish.sh
@@ -4,7 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly version=$(cat VERSION)
+readonly root_dir=$(cd `dirname $0`/../../.. && pwd)
+
+readonly version=$(cat ${root_dir}/VERSION)
 readonly git_branch="${1:-}"
 readonly git_sha=$(git rev-parse HEAD)
 readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
--- kafka-provisioner/.github/workflows/publish.sh
+++ kafka-provisioner/.github/workflows/publish.sh
@@ -8,8 +8,9 @@ gcloud config set disable_prompts True
 gcloud auth activate-service-account --key-file <(echo ${GCLOUD_CLIENT_SECRET} | base64 --decode)
 gcloud auth configure-docker
 
-readonly root=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." >/dev/null 2>&1 && pwd)
-readonly version=$(cat ${root}/VERSION)
+readonly root_dir=$(cd `dirname $0`/../../.. && pwd)
+
+readonly version=$(cat ${root_dir}/VERSION)
 readonly git_branch=${GITHUB_REF:11} # drop 'refs/head/' prefix
 readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
 readonly slug=${version}-${git_timestamp}-${GITHUB_SHA:0:16}
