f85097ef96a8e528dd98edbafd12ee839cfc9c2b
Scott Andrews <sandrews@pivotal.io>
2020-02-27 11:38:54 -0500

    fix ko tool install

--- fats/tools/ko.sh
+++ fats/tools/ko.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+ko_version="${1:-v0.3.0}"
+
 # avoid installing in the current directory since that may be a module
-(cd $TMPDIR && GO111MODULE=on go get github.com/google/ko/cmd/ko)
+(cd .. && GO111MODULE=on go get github.com/google/ko/cmd/ko@${ko_version})
