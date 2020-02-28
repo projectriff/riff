747109e9dbe92d15ce94daf0974f6ae3af476463
Scott Andrews <sandrews@pivotal.io>
2020-02-28 17:20:33 -0500

    typo

--- fats/macros/create-riff-dev-pod.sh
+++ fats/macros/create-riff-dev-pod.sh
@@ -4,5 +4,5 @@ riff_dev_version=${riff_dev_version:-0.6.0-snapshot}
 
 kubectl create serviceaccount riff-dev --namespace=${NAMESPACE}
 kubectl create rolebinding riff-dev-edit --namespace=${NAMESPACE} --clusterrole=edit --serviceaccount=${NAMESPACE}:riff-dev
-kubectl run riff-dev --namespace=${NAMESPACE} --image=projectriff/dev-utils:${utils_version} --serviceaccount=riff-dev --generator=run-pod/v1
+kubectl run riff-dev --namespace=${NAMESPACE} --image=projectriff/dev-utils:${riff_dev_version} --serviceaccount=riff-dev --generator=run-pod/v1
 kubectl wait pods riff-dev --for=condition=Ready --namespace=$NAMESPACE --timeout=60s
