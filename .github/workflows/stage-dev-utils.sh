94822e0ebed8bccc67e2fa23087efbc8e9f01508
Scott Andrews <sandrews@pivotal.io>
2020-02-28 19:08:46 -0500

    don't publish dev utils image for PRs

--- .github/workflows/stage-dev-utils.sh
+++ .github/workflows/stage-dev-utils.sh
@@ -4,10 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-GOOS=linux GOARCH=amd64 go build -o bin/publish ./cmd/publish
-GOOS=linux GOARCH=amd64 go build -o bin/subscribe ./cmd/subscribe
-
 image=projectriff/dev-utils:${VERSION_SLUG}
 
+GOOS=linux GOARCH=amd64 go build -o bin/publish ./cmd/publish
+GOOS=linux GOARCH=amd64 go build -o bin/subscribe ./cmd/subscribe
 docker build . -t ${image}
-docker push ${image}
+
+if [ $STAGE = "remote" ]; then
+  docker push ${image}
+fi
